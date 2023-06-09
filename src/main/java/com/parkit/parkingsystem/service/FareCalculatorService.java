8600798c9d02e36360e0a6dccf47279ba2dea706
ZinebELHALLAOUI <zinebhellaoui@gmail.com>
2023-06-10 01:51:12 +0200

    Ajout de fonctionnalité 30 premiére minutes gratuite

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,9 +5,9 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    public void calculateFare(Ticket ticket) {
+        if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
         long inHour = ticket.getInTime().getTime();
@@ -15,7 +15,12 @@ public class FareCalculatorService {
 
         long duration = outHour - inHour;
 
-        switch (ticket.getParkingSpot().getParkingType()){
+        if (duration < (30 * 60 * 1000)) {
+            ticket.setPrice(0);
+            return;
+        }
+
+        switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
                 ticket.setPrice((duration * Fare.CAR_RATE_PER_HOUR) / 3_600_000);
                 break;
@@ -24,7 +29,8 @@ public class FareCalculatorService {
                 ticket.setPrice((duration * Fare.BIKE_RATE_PER_HOUR) / 3_600_000);
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default:
+                throw new IllegalArgumentException("Unkown Parking Type");
         }
     }
 }
